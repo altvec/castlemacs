f590ba60e9980a01d81a952f53ba01265227ea3d
altvec <me@sergeykalistratov.com>
2018-09-23 20:39:29 +0500

    private config

--- private.el
+++ private.el
@@ -1,2 +1,36 @@
 ;; This is your private configuration file. It is loaded automatically, so feel free to add whatever you want.
 ;; This file will not be affected by Castlemacs updates.
+
+;; Replace default font
+(set-face-attribute 'default nil :font "Fira Code 14")
+
+
+;; Please stop making noises
+(defun my-bell-function ())
+
+(setq ring-bell-function 'my-bell-function)
+(setq visible-bell nil)
+
+
+;; Mode line
+;(use-package mode-icons
+;  :ensure t
+;  :config
+;  (mode-icons-mode t))
+
+;; Clojure ecosystem
+(use-package cider
+  :ensure t
+  :pin melpa-stable
+
+  :config
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (setq cider-repl-use-pretty-printing t)
+  (setq cider-repl-display-help-banner nil)
+
+  :bind (("M-r" . cider-namespace-refresh)
+         ("C-c r" . cider-repl-reset)
+         ("C-c ." . cider-reset-test-run-tests))
+  )
