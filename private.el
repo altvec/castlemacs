9d38e4bab5fb41c9b65aff19ecb9b9ca03dfc9df
altvec <me@sergeykalistratov.com>
2018-10-12 22:35:05 +0500

    Building JS IDE...

--- private.el
+++ private.el
@@ -124,6 +124,25 @@
   :ensure t)
 
 
+;; Javascript
+(use-package js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(use-package js2-refactor)
+(use-package xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js-mode-map (kbd "C-k") #'js2r-kill)
+
+(define-key js-mode-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook (lambda ()
+                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+
 ;; ========
 ;; ORG MODE
 
